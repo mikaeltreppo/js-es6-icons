055bdb832f1686903953cc9245bd2b0f04ce44ee
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-13 12:30:25 +0100

    Esercizio terminato

--- js/main.js
+++ js/main.js
@@ -133,42 +133,45 @@ selectDom.addEventListener('change',
 			containerDom.innerHTML = "";
 			let animalLeft = items.filter(anim => anim.type == "animal");
 			console.log(animalLeft);
-			for (let i = 0; i < animalLeft.length; i++){
-			containerDom.innerHTML +=  `<div id="ms_card" class="card mt-5">
+			for (let i = 0; i < animalLeft.length; i++) {
+				containerDom.innerHTML += `<div id="ms_card" class="card mt-5">
 			<div class="card-body d-flex flex-wrap justify-content-center">
 			<div class="w-100 text-center"><i class="fs-2 fa-solid ${animalLeft[i].prefix}${animalLeft[i].name}" style="color:${animalLeft[i].color}"></i></div>
 			<div>${animalLeft[i].name}</div>
 			</div>
 		  </div>`;
-		}}
+			}
+		}
 		//vegetables 
 		else if (selectDom.value == "vegetable") {
 			containerDom.innerHTML = "";
 			let vegeLeft = items.filter(veg => veg.type == "vegetable");
 			console.log(vegeLeft);
-			for (let i = 0; i < vegeLeft.length; i++){
-			containerDom.innerHTML +=  `<div id="ms_card" class="card mt-5">
+			for (let i = 0; i < vegeLeft.length; i++) {
+				containerDom.innerHTML += `<div id="ms_card" class="card mt-5">
 			<div class="card-body d-flex flex-wrap justify-content-center">
 			<div class="w-100 text-center"><i class="fs-2 fa-solid ${vegeLeft[i].prefix}${vegeLeft[i].name}" style="color:${vegeLeft[i].color}"></i></div>
 			<div>${vegeLeft[i].name}</div>
 			</div>
 		  </div>`;
-		}}
+			}
+		}
 		//user
 		else if (selectDom.value == "user") {
 			containerDom.innerHTML = "";
 			let userLeft = items.filter(user => user.type == "user");
 			console.log(userLeft);
-			for (let i = 0; i < userLeft.length; i++){
-			containerDom.innerHTML +=  `<div id="ms_card" class="card mt-5">
+			for (let i = 0; i < userLeft.length; i++) {
+				containerDom.innerHTML += `<div id="ms_card" class="card mt-5">
 			<div class="card-body d-flex flex-wrap justify-content-center">
 			<div class="w-100 text-center"><i class="fs-2 fa-solid ${userLeft[i].prefix}${userLeft[i].name}" style="color:${userLeft[i].color}"></i></div>
 			<div>${userLeft[i].name}</div>
 			</div>
 		  </div>`;
-		}}
+			}
+		}
 		// resto
-		else{
+		else {
 			for (let i = 0; i < items.length; i++) {
 				containerDom.innerHTML += `<div id="ms_card" class="card mt-5">
 			  <div class="card-body d-flex flex-wrap justify-content-center">
